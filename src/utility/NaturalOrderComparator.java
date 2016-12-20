662ed89d1ed626b3fc3029c91a889ff5e120dcd5
Jan Motl <jan@motl.us>
2016-12-20 14:30:54 +0100

    NaturalOrdering

--- src/utility/NaturalOrderComparator.java
+++ src/utility/NaturalOrderComparator.java
@@ -1,201 +1,89 @@
 package utility;
 
 /*
- NaturalOrderComparator.java -- Perform 'natural order' comparisons of strings in Java.
- Copyright (C) 2003 by Pierre-Luc Paour <natorder@paour.com>
- Based on the C version by Martin Pool, of which this is more or less a straight conversion.
- Copyright (C) 2000 by Martin Pool <mbp@humbug.org.au>
- This software is provided 'as-is', without any express or implied
- warranty.  In no event will the authors be held liable for any damages
- arising from the use of this software.
- Permission is granted to anyone to use this software for any purpose,
- including commercial applications, and to alter it and redistribute it
- freely, subject to the following restrictions:
- 1. The origin of this software must not be misrepresented; you must not
- claim that you wrote the original software. If you use this software
- in a product, an acknowledgment in the product documentation would be
- appreciated but is not required.
- 2. Altered source versions must be plainly marked as such, and must not be
- misrepresented as being the original software.
- 3. This notice may not be removed or altered from any source distribution.
+ * Copyright (c) 2007 Eric Berry <elberry@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 
-/*
- Notes by Jan Motl.
- The implementation dislikes nulls at the input.
- If this sort ends up to be unintuitive, replace this class with some other from:
-    http://stackoverflow.com/questions/1262239/natural-sort-order-string-comparison-in-java-is-one-built-in
- If you decide to make "case-ignoring", make sure "TABLE" and "table" are not going to clash in maps (these two names
- can appear in case-sensitive database). Hence, do not implement case-insensitive but something like "case is of low
- importance".
-*/
+// Modified by Jan Motl to give case difference a low priority (taken care by util.text.Collator).
+// Alternatives:
+//  NaturalOrderComparator by Pierre-Luc Paour
+//      http://stackoverflow.com/questions/1262239/natural-sort-order-string-comparison-in-java-is-one-built-in
+//      Treats nicely repeated spaces, but is difficult to modify to give the case low priority
 
-import java.util.Arrays;
-import java.util.Collections;
+
+import java.text.Collator;
+import java.text.DecimalFormatSymbols;
 import java.util.Comparator;
-import java.util.List;
 
-public class NaturalOrderComparator implements Comparator<Object>   // Modified to avoid unchecked warning
-{
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-    public int compare(Object o1, Object o2)
-    {
-        String a = o1.toString();
-        String b = o2.toString();
+/**
+ * Compares Strings by human values instead of traditional machine values.
+ * @author elberry
+ */
+public class NaturalOrderComparator implements Comparator<String> {
 
-        int ia = 0, ib = 0;
-        int nza = 0, nzb = 0;
-        char ca, cb;
-        int result;
+	private Pattern alphaNumChunkPattern;
 
-        while (true)
-        {
-            // only count the number of zeroes leading the last number compared
-            nza = nzb = 0;
+	public NaturalOrderComparator() {
+		this(Locale.getDefault());
+	}
 
-            ca = charAt(a, ia);
-            cb = charAt(b, ib);
+	public NaturalOrderComparator(Locale locale) {
+		DecimalFormatSymbols dfs = new DecimalFormatSymbols(locale);
+		char localeDecimalSeparator = dfs.getDecimalSeparator();
+		// alphaNumChunkPatter initialized here to get correct decimal separator for locale.
+		alphaNumChunkPattern = Pattern.compile("(\\d+\\" + localeDecimalSeparator + "\\d+)|(\\d+)|(\\D+)");
+	}
 
-            // skip over leading spaces or zeros
-            while (Character.isSpaceChar(ca) || ca == '0')
-            {
-                if (ca == '0')
-                {
-                    nza++;
-                }
-                else
-                {
-                    // only count consecutive zeroes
-                    nza = 0;
-                }
-
-                ca = charAt(a, ++ia);
-            }
-
-            while (Character.isSpaceChar(cb) || cb == '0')
-            {
-                if (cb == '0')
-                {
-                    nzb++;
-                }
-                else
-                {
-                    // only count consecutive zeroes
-                    nzb = 0;
-                }
-
-                cb = charAt(b, ++ib);
-            }
-
-            // process run of digits
-            if (Character.isDigit(ca) && Character.isDigit(cb))
-            {
-                if ((result = compareRight(a.substring(ia), b.substring(ib))) != 0)
-                {
-                    return result;
-                }
-            }
-
-            if (ca == 0 && cb == 0)
-            {
-                // The strings compare the same. Perhaps the caller
-                // will want to call strcmp to break the tie.
-                return nza - nzb;
-            }
-
-            if (ca < cb)
-            {
-                return -1;
-            }
-            else if (ca > cb)
-            {
-                return +1;
-            }
-
-            ++ia;
-            ++ib;
-        }
-    }
-
-    static char charAt(String s, int i)
-    {
-        if (i >= s.length())
-        {
-            return 0;
-        }
-        else
-        {
-            return s.charAt(i);
-        }
-    }
-
-    static int compareRight(String a, String b)
-    {
-        int bias = 0;
-        int ia = 0;
-        int ib = 0;
-
-        // The longest run of digits wins. That aside, the greatest
-        // value wins, but we can't know that it will until we've scanned
-        // both numbers to know that they have the same magnitude, so we
-        // remember it in BIAS.
-        for (;; ia++, ib++)
-        {
-            char ca = charAt(a, ia);
-            char cb = charAt(b, ib);
-
-            if (!Character.isDigit(ca) && !Character.isDigit(cb))
-            {
-                return bias;
-            }
-            else if (!Character.isDigit(ca))
-            {
-                return -1;
-            }
-            else if (!Character.isDigit(cb))
-            {
-                return +1;
-            }
-            else if (ca < cb)
-            {
-                if (bias == 0)
-                {
-                    bias = -1;
-                }
-            }
-            else if (ca > cb)
-            {
-                if (bias == 0)
-                    bias = +1;
-            }
-            else if (ca == 0 && cb == 0)
-            {
-                return bias;
-            }
-        }
-    }
-
-    public static void main(String[] args)
-    {
-        String[] strings = new String[] { "1-2", "1-02", "1-20", "10-20", "fred", "jane", "pic01",
-                "pic2", "pic02", "pic02a", "pic3", "pic4", "pic 4 else", "pic 5", "pic05", "pic 5",
-                "pic 5 something", "pic 6", "pic   7", "pic100", "pic100a", "pic120", "pic121",
-                "pic02000", "tom", "x2-g8", "x2-y7", "x2-y08", "x8-y8" };
-
-        strings = new String[] {"MUTA_44", "muta_44", "muta_188", "muta_32"};
-
-        List orig = Arrays.asList(strings);
-
-        System.out.println("Original: " + orig);
-
-        List scrambled = Arrays.asList(strings);
-        Collections.shuffle(scrambled);
-
-        System.out.println("Scrambled: " + scrambled);
-
-        Collections.sort(scrambled, new NaturalOrderComparator());
-
-        System.out.println("Sorted: " + scrambled);
-    }
-
+	public int compare(String s1, String s2) {
+		int compareValue = 0;
+		Matcher s1ChunkMatcher = alphaNumChunkPattern.matcher(s1);
+		Matcher s2ChunkMatcher = alphaNumChunkPattern.matcher(s2);
+		Collator collator = Collator.getInstance(); // Use the default locale on the computer for accent ordering
+		String s1ChunkValue = null;
+		String s2ChunkValue = null;
+		while (s1ChunkMatcher.find() && s2ChunkMatcher.find() && compareValue == 0) {
+			s1ChunkValue = s1ChunkMatcher.group();
+			s2ChunkValue = s2ChunkMatcher.group();
+			try {
+				// compare double values - ints get converted to doubles. Eg. 100 = 100.0
+				Double s1Double = Double.valueOf(s1ChunkValue);
+				Double s2Double = Double.valueOf(s2ChunkValue);
+				compareValue = s1Double.compareTo(s2Double);
+			} catch (NumberFormatException ignored) {
+				// not a number, use string comparison.
+				compareValue = collator.compare(s1ChunkValue, s2ChunkValue);
+			}
+			// if they are equal thus far, but one has more left, it should come after the one that doesn't.
+			if (compareValue == 0) {
+				if (s1ChunkMatcher.hitEnd() && !s2ChunkMatcher.hitEnd()) {
+					compareValue = -1;
+				} else if (!s1ChunkMatcher.hitEnd() && s2ChunkMatcher.hitEnd()) {
+					compareValue = 1;
+				}
+			}
+		}
+		return compareValue;
+	}
 }
--- test/utility/NaturalOrderComparatorTest.java
+++ test/utility/NaturalOrderComparatorTest.java
@@ -11,13 +11,43 @@ import java.util.TreeSet;
 public class NaturalOrderComparatorTest {
 
 	@Test
-	public void orderingTest() {
+	public void naturalOrdering() {
 		String[] strings = new String[] {"muta_44", "muta_188", "muta_32"};
 
 		Arrays.sort(strings, new NaturalOrderComparator());
 
-		Assert.assertArrayEquals(new String[]{"muta_32", "muta_44", "muta_188"}, strings);
+		Assert.assertArrayEquals(new String[] {"muta_32", "muta_44", "muta_188"}, strings);
     }
+
+	@Test
+	public void accentCasePriority() {
+		String[] strings = new String[] {"Äbc", "äbc", "Àbc", "àbc", "Abc", "abc", "ABC"};
+
+		Arrays.sort(strings, new NaturalOrderComparator());
+
+		// First by basic, second by accent, third by case
+		Assert.assertArrayEquals(new String[] {"abc", "Abc", "ABC", "àbc", "Àbc", "äbc", "Äbc"}, strings);
+	}
+
+	@Test
+	public void singleSpace() {
+		String[] strings = new String[] {"m 69", "m 07", "m 6"};
+
+		Arrays.sort(strings, new NaturalOrderComparator());
+
+		// Unfortunately, this implementation does not correctly sort {"m 69", "m     07", "m 6"}
+		Assert.assertArrayEquals(new String[] {"m 6", "m 07", "m 69"}, strings);
+	}
+
+	@Test
+	public void suffix() {
+		String[] strings = new String[] {"m13", "m12", "m12 a", "m12 b", "m11"};
+
+		Arrays.sort(strings, new NaturalOrderComparator());
+
+		Assert.assertArrayEquals(new String[] {"m11", "m12", "m12 a", "m12 b", "m13"}, strings);
+	}
+
 
 	@Test
 	public void notEqualTest() {
